1f27375e6d8e8af3eeb68222e2bea8dfdefb2275
Otavio Tampellini <tampelliniotavio@gmail.com>
2024-12-04 17:14:15 -0300

    feat: password hash and verify

---
+++ internal/infrastructure/encrypt/hash_test.go
@@ -0,0 +1,29 @@
+package encrypt
+
+import (
+	"testing"
+
+    "github.com/jaswdr/faker/v2"
+	"github.com/stretchr/testify/assert"
+)
+
+var (
+    password = "randomPassword"
+)
+
+func Test_hashPassword_should_encrypt (t *testing.T) {
+    assert := assert.New(t)
+
+    hashed, err := HashPassword(password)
+
+    assert.Nil(err)
+    assert.True(hashed != password)
+}
+
+func Test_hashPassword_should_error (t *testing.T) {
+    assert := assert.New(t)
+
+    _, err := HashPassword(faker.New().Lorem().Text(100))
+
+    assert.NotNil(err)
+}
