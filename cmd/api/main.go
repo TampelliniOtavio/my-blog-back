73b92dbdfa026f311917cb84f9451415ab0fdf90
Otavio Tampellini <tampelliniotavio@gmail.com>
2024-12-14 12:07:06 -0300

    refactor(main.go): better routes print

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,32 +5,75 @@ import (
 
 	"github.com/TampelliniOtavio/my-blog-back/internal/endpoints"
 	"github.com/TampelliniOtavio/my-blog-back/internal/infrastructure/database"
+	"github.com/fatih/color"
 	"github.com/goccy/go-json"
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-    err := godotenv.Load()
+	err := godotenv.Load()
 
-    db := database.NewDB()
-    defer db.Close()
+	db := database.NewDB()
+	defer db.Close()
 
-    if err != nil {
-        panic(err)
-    }
+	if err != nil {
+		panic(err)
+	}
 
-    app := fiber.New(fiber.Config{
-        AppName: "Backend My Blog",
-        JSONEncoder: json.Marshal,
-	    JSONDecoder: json.Unmarshal,
-        ErrorHandler: endpoints.ErrorHandler,
-    })
+	app := fiber.New(fiber.Config{
+		AppName:      "Backend My Blog",
+		JSONEncoder:  json.Marshal,
+		JSONDecoder:  json.Unmarshal,
+		ErrorHandler: endpoints.ErrorHandler,
+	})
 
-    endpoints.DefineRoutes(app, db)
+	endpoints.DefineRoutes(app, db)
 
-    data, _ := json.MarshalIndent(app.Stack(), "", "  ")
-    fmt.Println(string(data))
+	printRoutes(app)
 
-    app.Listen(":3000")
+	app.Listen(":3000")
+}
+
+func printRoutes(app *fiber.App) {
+	index := 0
+
+	for _, routes := range app.Stack() {
+		for _, route := range routes {
+			fmt.Printf(
+				"%d %s %s %s\n",
+				index,
+				colorizeMethod(route.Method),
+				route.Path,
+				route.Name,
+			)
+			index++
+		}
+	}
+}
+
+func colorizeMethod(method string) string {
+	green := color.New(color.Bold, color.FgGreen).SprintFunc()
+	blue := color.New(color.Bold, color.FgBlue).SprintFunc()
+	red := color.New(color.Bold, color.FgRed).SprintFunc()
+	yellow := color.New(color.Bold, color.FgYellow).SprintFunc()
+	cyan := color.New(color.Bold, color.FgCyan).SprintFunc()
+
+	if method == "GET" || method == "HEAD" {
+		return yellow(method)
+	}
+
+	if method == "POST" {
+		return green(method)
+	}
+
+	if method == "PUT" || method == "PATCH" {
+		return blue(method)
+	}
+
+	if method == "DELETE" {
+		return red(method)
+	}
+
+	return cyan(method)
 }
--- go.mod
+++ go.mod
@@ -3,9 +3,12 @@ module github.com/TampelliniOtavio/my-blog-back
 go 1.23.0
 
 require (
+	github.com/fatih/color v1.18.0
 	github.com/go-playground/validator/v10 v10.23.0
 	github.com/goccy/go-json v0.10.3
+	github.com/gofiber/contrib/jwt v1.0.10
 	github.com/gofiber/fiber/v2 v2.52.5
+	github.com/golang-jwt/jwt/v5 v5.2.1
 	github.com/jaswdr/faker/v2 v2.3.3
 	github.com/jmoiron/sqlx v1.4.0
 	github.com/joho/godotenv v1.5.1
@@ -22,8 +25,6 @@ require (
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
-	github.com/gofiber/contrib/jwt v1.0.10 // indirect
-	github.com/golang-jwt/jwt/v5 v5.2.1 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/klauspost/compress v1.17.11 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
